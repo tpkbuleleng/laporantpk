095d3cc0aed1196e38ff780282c81d5ea6c40d63
tpkbuleleng <admin@ipekbbuleleng.com>
2026-03-13 09:01:02 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tpk-buleleng-v13';
+const CACHE_NAME = 'tpk-buleleng-v20';
 const ASSETS = [
   './',
   './index.html',
@@ -38,6 +38,7 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
 
 
 
